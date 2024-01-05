ba86a490f5e8369efabdca4717fbb21c53428764
the-gronkler <alexsvirin1337@gmail.com>
2024-01-05 13:25:53 +0100

    further improved documentation

--- src/Client/ClientMain.java
+++ src/Client/ClientMain.java
@@ -1,15 +1,16 @@
 package client;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Main class for the client application.
- * Instantiates a {@link ClientGUI} object on the AWT event dispatch thread
+ * Instantiates a {@link ClientGUI} object on the {@linkplain EventQueue AWT Event Dispatch Thread}
  */
 public class ClientMain {
     /**
      * Main method for the client application.
-     * Instantiates a {@link ClientGUI} object on the AWT event dispatch thread
+     * Instantiates a {@link ClientGUI} object on the {@linkplain EventQueue AWT Event Dispatch Thread}.
      */
     public static void main(String[] args)  {
         SwingUtilities.invokeLater( ClientGUI::new);
